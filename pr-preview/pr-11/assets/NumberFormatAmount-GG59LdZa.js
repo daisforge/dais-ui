43e6e67ff79959fac7c9afe2434192d164e6b329
RamK-16 <RamK-16@users.noreply.github.com>
2026-08-11 09:26:33 +0000

    Remove preview for PR 11 🛬

--- pr-preview/pr-11/assets/NumberFormatAmount-GG59LdZa.js
+++
@@ -1,5 +0,0 @@
-import{r as a,d as e}from"./react-D2T61mpp.js";import{N as s}from"./NumberFormat-BlTr3jyf.js";import{ap as p,Y as n}from"./@salutejs/sdds-themes-DMMPng_c.js";import{H as u,C as i}from"./styled-components-CxmyZpOq.js";const d={xs:n,s:p},c=o=>i(d[o]),N=u(s)`
-  && .input-wrapper div:has(> input) {
-    ${({$amountSize:o})=>c(o)}
-  }
-`,r=a.forwardRef(({size:o="xs",...t},m)=>e.jsxDEV(N,{ref:m,$amountSize:o,size:o,...t},void 0,!1,{fileName:"/home/runner/work/dais-ui/dais-ui/packages/ui-kit/src/components/NumberFormatAmount/NumberFormatAmount.tsx",lineNumber:12,columnNumber:3},void 0));r.displayName="NumberFormatAmount";try{r.displayName="NumberFormatAmount",r.__docgenInfo={description:"",displayName:"NumberFormatAmount",props:{}}}catch{}export{r as N};
--- pr-preview/pr-11/assets/Table.infinityScrollAPI-BWAFF3Kj.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{c2 as i,c3 as r}from"./vendor-DVRMjF5Y.js";import{T as s}from"./TypeSourceViewer-Be1t8e_y.js";import"./react-is-Clcustum.js";import"./styled-components-CxmyZpOq.js";import"./tslib-De9GV7Vy.js";function o(n){const e={h1:"h1",h2:"h2",...i(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(r,{title:"Локальные компоненты/Table/InfinityScroll/API"}),`
-`,t.jsx(e.h1,{id:"infinity-scroll-api",children:"Infinity Scroll API"}),`
-`,t.jsx(e.h2,{id:"tableconfig",children:"TableConfig"}),`
-`,t.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/components/Table/types/table-config.type.ts",typeName:"TableConfig"})]})}function h(n={}){const{wrapper:e}={...i(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(o,{...n})}):o(n)}export{h as default};
