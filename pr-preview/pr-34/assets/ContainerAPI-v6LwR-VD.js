d46640ce813f821d3849f864f26c733c964a3e57
RamK-16 <RamK-16@users.noreply.github.com>
2026-08-21 17:04:43 +0000

    Remove preview for PR 34 🛬

--- pr-preview/pr-34/assets/ContainerAPI-v6LwR-VD.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./react-D2T61mpp.js";import{c2 as r,c3 as a}from"./vendor-CU5ziH9B.js";import{T as i}from"./TypeSourceViewer-CA9R_L0n.js";import"./react-is-Clcustum.js";import"./styled-components-C2-iHwga.js";import"./tslib-De9GV7Vy.js";function o(t){const e={h1:"h1",h2:"h2",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Композиции/Container/API"}),`
-`,n.jsx(e.h1,{id:"container-api",children:"Container API"}),`
-`,n.jsx(e.h2,{id:"containerprops",children:"ContainerProps"}),`
-`,n.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/layouts/Container/Container.types.ts",typeName:"ContainerProps"}),`
-`,n.jsx(e.h2,{id:"tcontainerview",children:"TContainerView"}),`
-`,n.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/layouts/Container/Container.types.ts",typeName:"TContainerView"})]})}function l(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{l as default};
--- pr-preview/pr-34/assets/rehype-sanitize-CdjI7ORf.js
+++
@@ -1,1 +0,0 @@
-import{bk as e}from"./vendor-CU5ziH9B.js";function o(r){return function(t){return e(t,r)}}export{o as r};
