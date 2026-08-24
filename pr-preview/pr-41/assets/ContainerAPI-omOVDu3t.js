8cf0bb19c2f3a3a2f2cede385078d79540a3bc45
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-08-24 17:54:38 +0000

    Remove preview for PR 41 🛬

--- pr-preview/pr-41/assets/ContainerAPI-omOVDu3t.js
+++
@@ -1,6 +0,0 @@
-import{j as n}from"./react-D2T61mpp.js";import{c4 as r,c5 as a}from"./vendor-4DQodAhx.js";import{T as i}from"./TypeSourceViewer-BEDFerHI.js";import"./react-is-Clcustum.js";import"./styled-components-rNTPyvwi.js";import"./tslib-De9GV7Vy.js";function o(t){const e={h1:"h1",h2:"h2",...r(),...t.components};return n.jsxs(n.Fragment,{children:[n.jsx(a,{title:"Композиции/Container/API"}),`
-`,n.jsx(e.h1,{id:"container-api",children:"Container API"}),`
-`,n.jsx(e.h2,{id:"containerprops",children:"ContainerProps"}),`
-`,n.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/layouts/Container/Container.types.ts",typeName:"ContainerProps"}),`
-`,n.jsx(e.h2,{id:"tcontainerview",children:"TContainerView"}),`
-`,n.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/layouts/Container/Container.types.ts",typeName:"TContainerView"})]})}function l(t={}){const{wrapper:e}={...r(),...t.components};return e?n.jsx(e,{...t,children:n.jsx(o,{...t})}):o(t)}export{l as default};
--- pr-preview/pr-41/assets/TableCanvas.cellPreviewAPI-CX3uGBuA.js
+++
@@ -1,7 +0,0 @@
-import{j as e}from"./react-D2T61mpp.js";import{c4 as o,c5 as l}from"./vendor-4DQodAhx.js";import{T as t}from"./TypeSourceViewer-BEDFerHI.js";import"./react-is-Clcustum.js";import"./styled-components-rNTPyvwi.js";import"./tslib-De9GV7Vy.js";function i(r){const n={code:"code",h1:"h1",h2:"h2",p:"p",...o(),...r.components};return e.jsxs(e.Fragment,{children:[e.jsx(l,{title:"Локальные компоненты/TableCanvas/Cell Preview/API"}),`
-`,e.jsx(n.h1,{id:"cell-preview-api",children:"Cell Preview API"}),`
-`,e.jsx(n.h2,{id:"rendercellpreview-поле-columnconfig",children:"renderCellPreview (поле ColumnConfig)"}),`
-`,e.jsxs(n.p,{children:["Опциональное поле в конфигурации колонки (",e.jsx(n.code,{children:"ColumnConfig"}),")."]}),`
-`,e.jsx(t,{filePath:"packages/ui-kit/src/components/TableCanvas/types/column-config.type.ts",typeName:"RenderCellPreview",language:"ts"}),`
-`,e.jsx(n.h2,{id:"previewcellinfo-аргумент-функции-rendercellpreview",children:"PreviewCellInfo (аргумент функции renderCellPreview)"}),`
-`,e.jsx(t,{filePath:"packages/ui-kit/src/components/TableCanvas/types/column-config.type.ts",typeName:"PreviewCellInfo",language:"ts"})]})}function u(r={}){const{wrapper:n}={...o(),...r.components};return n?e.jsx(n,{...r,children:e.jsx(i,{...r})}):i(r)}export{u as default};
