e8e3664f68c91cc83f071dba38756927b1eb98da
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-08-27 06:57:16 +0000

    Remove preview for PR 44 🛬

--- pr-preview/pr-44/assets/DocStoryTemplate-Amd1X9tE.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{c4 as o,c5 as s,b_ as a}from"./vendor-79A0Y1rr.js";import"./react-is-Clcustum.js";import"./styled-components-CyG4-HBp.js";import"./tslib-De9GV7Vy.js";function r(n){return t.jsxs(t.Fragment,{children:[`
-`,`
-`,`
-`,t.jsx(s,{isTemplate:!0}),`
-`,t.jsx(a,{})]})}function x(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r()}export{x as default};
--- pr-preview/pr-44/assets/rehype-sanitize-Co1FS-I6.js
+++
@@ -1,1 +0,0 @@
-import{bm as e}from"./vendor-79A0Y1rr.js";function o(r){return function(t){return e(t,r)}}export{o as r};
