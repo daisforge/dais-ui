3d2fb591f544c8497114c5a1900a652e768821fd
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-08-28 10:23:50 +0000

    Remove preview for PR 47 🛬

--- pr-preview/pr-47/assets/CanvasIconButtonAPI-DU3bC0rQ.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{c6 as o,c7 as i}from"./vendor-DFDWE34s.js";import{T as s}from"./TypeSourceViewer-BGAwPKqv.js";import"./react-is-Clcustum.js";import"./styled-components-sb3aauQH.js";import"./tslib-De9GV7Vy.js";function a(e){const n={h1:"h1",h2:"h2",...o(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Локальные компоненты/TableCanvas/CanvasElements/CanvasIconButton/API"}),`
-`,t.jsx(n.h1,{id:"canvasiconbutton-api",children:"Canvas.IconButton API"}),`
-`,t.jsx(n.h2,{id:"canvasiconbuttonoptions",children:"CanvasIconButtonOptions"}),`
-`,t.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/components/TableGlide/lib/canvas/primitives/CanvasIconButton.ts",typeName:"CanvasIconButtonOptions"}),`
-`,t.jsx(n.h2,{id:"buttonview",children:"ButtonView"}),`
-`,t.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/components/TableGlide/lib/canvas/cells/buttons/types.ts",typeName:"ButtonView"}),`
-`,t.jsx(n.h2,{id:"buttonsize",children:"ButtonSize"}),`
-`,t.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/components/TableGlide/lib/canvas/cells/buttons/types.ts",typeName:"ButtonSize"})]})}function h(e={}){const{wrapper:n}={...o(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(a,{...e})}):a(e)}export{h as default};
