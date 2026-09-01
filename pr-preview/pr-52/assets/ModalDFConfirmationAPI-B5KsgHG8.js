81379c47bd803b2c2fc219fc8c5f905fcd76c39d
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-01 06:57:46 +0000

    Remove preview for PR 52 🛬

--- pr-preview/pr-52/assets/ModalDFConfirmationAPI-B5KsgHG8.js
+++
@@ -1,6 +0,0 @@
-import{j as o}from"./react-D2T61mpp.js";import{c6 as e,c7 as r}from"./vendor-C18rJQsW.js";import{T as i}from"./TypeSourceViewer-CLZbMNMb.js";import"./react-is-Clcustum.js";import"./styled-components-z7m5HNHq.js";import"./tslib-De9GV7Vy.js";function a(n){const t={h1:"h1",h2:"h2",...e(),...n.components};return o.jsxs(o.Fragment,{children:[o.jsx(r,{title:"Локальные компоненты/ModalDFConfirmation/API"}),`
-`,o.jsx(t.h1,{id:"modaldfconfirmation-api",children:"ModalDFConfirmation API"}),`
-`,o.jsx(t.h2,{id:"modaldfconfirmationprops",children:"ModalDFConfirmationProps"}),`
-`,o.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/components/ModalDFConfirmation/ModalDFConfirmation.tsx",typeName:"ModalDFConfirmationProps"}),`
-`,o.jsx(t.h2,{id:"modaldfconfirmationcontent",children:"ModalDFConfirmationContent"}),`
-`,o.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/components/ModalDFConfirmation/ModalDFConfirmation.tsx",typeName:"ModalDFConfirmationContent"})]})}function p(n={}){const{wrapper:t}={...e(),...n.components};return t?o.jsx(t,{...n,children:o.jsx(a,{...n})}):a(n)}export{p as default};
--- pr-preview/pr-52/assets/TooltipListAPI-BaZpPwIF.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./react-D2T61mpp.js";import{c6 as s,c7 as n}from"./vendor-C18rJQsW.js";import{T as r}from"./TypeSourceViewer-CLZbMNMb.js";import"./react-is-Clcustum.js";import"./styled-components-z7m5HNHq.js";import"./tslib-De9GV7Vy.js";function i(e){const t={blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",p:"p",...s(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{title:"Локальные компоненты/TooltipList/API"}),`
-`,o.jsx(t.h1,{id:"tooltiplist-api",children:"TooltipList API"}),`
-`,o.jsx(t.h2,{id:"tooltiplistprops",children:"TooltipListProps"}),`
-`,o.jsx(r,{language:"ts",filePath:"packages/ui-kit/src/components/Tooltip/TooltipList.tsx",typeName:"TooltipListProps"}),`
-`,o.jsxs(t.blockquote,{children:[`
-`,o.jsxs(t.p,{children:["Остальные пропсы наследуются от ",o.jsx(t.code,{children:"TooltipProps"})," (кроме ",o.jsx(t.code,{children:"text"}),", ",o.jsx(t.code,{children:"children"}),")"]}),`
-`]})]})}function h(e={}){const{wrapper:t}={...s(),...e.components};return t?o.jsx(t,{...e,children:o.jsx(i,{...e})}):i(e)}export{h as default};
