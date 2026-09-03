d36ded6d90a9e58c487295689371314244d9a9bf
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-03 08:46:29 +0000

    Remove preview for PR 54 🛬

--- pr-preview/pr-54/assets/DocStoryTemplate-oNxBpJHV.js
+++
@@ -1,5 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{c6 as o,c7 as s,c0 as a}from"./vendor-1keUuV-j.js";import"./react-is-Clcustum.js";import"./styled-components-D2iiFT0j.js";import"./tslib-De9GV7Vy.js";function r(n){return t.jsxs(t.Fragment,{children:[`
-`,`
-`,`
-`,t.jsx(s,{isTemplate:!0}),`
-`,t.jsx(a,{})]})}function x(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r()}export{x as default};
--- pr-preview/pr-54/assets/TableCanvas.editingAPI-Dl8tAJ7j.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{c6 as a,c7 as o}from"./vendor-1keUuV-j.js";import{T as i}from"./TypeSourceViewer-BaYu7dgG.js";import"./react-is-Clcustum.js";import"./styled-components-D2iiFT0j.js";import"./tslib-De9GV7Vy.js";function s(e){const n={h1:"h1",h2:"h2",...a(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(o,{title:"Локальные компоненты/TableCanvas/Editing/API"}),`
-`,t.jsx(n.h1,{id:"editing-api",children:"Editing API"}),`
-`,t.jsx(n.h2,{id:"editingconfig",children:"EditingConfig"}),`
-`,t.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/components/TableCanvas/types/table-config.type.ts",typeName:"EditingConfig"}),`
-`,t.jsx(n.h2,{id:"editingbuttonprops",children:"EditingButtonProps"}),`
-`,t.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/components/TableCanvas/types/table-config.type.ts",typeName:"EditingButtonProps"}),`
-`,t.jsx(n.h2,{id:"editing",children:"Editing"}),`
-`,t.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/components/TableCanvas/types/column-config.type.ts",typeName:"Editing"})]})}function l(e={}){const{wrapper:n}={...a(),...e.components};return n?t.jsx(n,{...e,children:t.jsx(s,{...e})}):s(e)}export{l as default};
