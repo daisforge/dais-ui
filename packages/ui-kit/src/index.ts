7de453c891f1783d4e2839ed181fa11cd581dd7d
Ramil Khovalyg <94189183+RamK-16@users.noreply.github.com>
2026-09-08 16:09:54 +0300

    feat: поднятие версии для синхронизации. Пустой минор-бамп: обновлен комментарий в index.ts

--- packages/ui-kit/src/index.ts
+++ packages/ui-kit/src/index.ts
@@ -1,4 +1,4 @@
-// components <//for rebuild republish: 1.17.0 — перевыпуск ради корректного mcp-data ------- delete!!!!!>
+// components <//for rebuild republish: 1.17.0 — minor >
 export * from './components/Accordion';
 export * from './components/AiAgentPopover';
 export * from './components/AnalyticalWidget';
