6cb6e57d8cda80fac0f6b3c7c6c5fc63e94731d7
15fps <48308248+PavelPotapov@users.noreply.github.com>
2026-09-14 13:30:52 +0300

    docs(TableCanvas): поправлен contentAlign в jsdoc MergedCellsAlign
    Дефолт horizontal у MergedCellsAlign ссылался на несуществующее поле contentAlign, исправлено на contentFormat.alignContent. Догоняет справочник CellsMerging (#56).

--- packages/ui-kit/src/components/TableCanvas/types/merged-cells.type.ts
+++ packages/ui-kit/src/components/TableCanvas/types/merged-cells.type.ts
@@ -1,6 +1,6 @@
 /** Выравнивание контента в объединённой ячейке тела таблицы. */
 export type MergedCellsAlign = {
-  /** По горизонтали. @default из `contentAlign` колонки, иначе 'left' */
+  /** По горизонтали. @default из `contentFormat.alignContent` колонки, иначе 'left' */
   horizontal?: 'left' | 'center' | 'right';
   /** По вертикали. @default 'center' */
   vertical?: 'top' | 'center' | 'bottom';
