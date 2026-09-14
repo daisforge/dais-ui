1b0ae3ccf8f2e9b81ea0b0013cc03aa3a8d257a4
RamK-16 <RamK-16@users.noreply.github.com>
2026-09-14 11:39:49 +0000

    Remove preview for PR 70 🛬

--- pr-preview/pr-70/assets/CanvasSkeletonAPI-CtBbN95X.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./react-D2T61mpp.js";import{cg as i,ch as o}from"./vendor-CYHm0xTY.js";import{T as t}from"./TypeSourceViewer-wHTU1SHV.js";import"./react-is-Clcustum.js";import"./styled-components-BWtOdjTE.js";import"./@tanstack/react-virtual-BjNtAbBb.js";import"./tslib-DoU9Jm1N.js";function a(s){const n={h1:"h1",h2:"h2",...i(),...s.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Локальные компоненты/TableCanvas/CanvasElements/CanvasSkeleton/API"}),`
-`,e.jsx(n.h1,{id:"canvasskeleton-api",children:"Canvas.Skeleton API"}),`
-`,e.jsx(n.h2,{id:"canvasskeletonoptions",children:"CanvasSkeletonOptions"}),`
-`,e.jsx(t,{language:"ts",filePath:"packages/ui-kit/src/components/TableGlide/lib/canvas/primitives/CanvasSkeleton.ts",typeName:"CanvasSkeletonOptions"}),`
-`,e.jsx(n.h2,{id:"roundness",children:"Roundness"}),`
-`,e.jsx(t,{language:"ts",filePath:"packages/ui-kit/src/components/TableGlide/lib/canvas/primitives/CanvasSkeleton.ts",typeName:"Roundness"}),`
-`,e.jsx(n.h2,{id:"roundness_map",children:"ROUNDNESS_MAP"}),`
-`,e.jsx(t,{language:"ts",filePath:"packages/ui-kit/src/components/TableGlide/lib/canvas/primitives/CanvasSkeleton.ts",typeName:"ROUNDNESS_MAP"})]})}function h(s={}){const{wrapper:n}={...i(),...s.components};return n?e.jsx(n,{...s,children:e.jsx(a,{...s})}):a(s)}export{h as default};
--- pr-preview/pr-70/assets/TableCanvas.splitIconButtonAPI-CfXTDe-1.js
+++
@@ -1,4 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{cg as s,ch as i}from"./vendor-CYHm0xTY.js";import{T as c}from"./TypeSourceViewer-wHTU1SHV.js";import"./react-is-Clcustum.js";import"./styled-components-BWtOdjTE.js";import"./@tanstack/react-virtual-BjNtAbBb.js";import"./tslib-DoU9Jm1N.js";function e(n){const o={h1:"h1",h2:"h2",...s(),...n.components};return t.jsxs(t.Fragment,{children:[t.jsx(i,{title:"Локальные компоненты/TableCanvas/ControlBlock/SplitIconButton/API"}),`
-`,t.jsx(o.h1,{id:"spliticonbutton-api",children:"SplitIconButton API"}),`
-`,t.jsx(o.h2,{id:"spliticonbuttonprops",children:"SplitIconButtonProps"}),`
-`,t.jsx(c,{language:"ts",filePath:"packages/ui-kit/src/components/TableCanvas/components/SplitIconButton/SplitIconButton.types.ts",typeName:"SplitIconButtonProps"})]})}function x(n={}){const{wrapper:o}={...s(),...n.components};return o?t.jsx(o,{...n,children:t.jsx(e,{...n})}):e(n)}export{x as default};
