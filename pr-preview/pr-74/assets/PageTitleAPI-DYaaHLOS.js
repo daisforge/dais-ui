69d50e8317116692266215bdade653cbc4d19367
RamK-16 <RamK-16@users.noreply.github.com>
2026-09-15 08:42:09 +0000

    Remove preview for PR 74 🛬

--- pr-preview/pr-74/assets/PageTitleAPI-DYaaHLOS.js
+++
@@ -1,8 +0,0 @@
-import{j as e}from"./react-D2T61mpp.js";import{cg as n,ch as o}from"./vendor-BpZdl-X2.js";import{T as s}from"./TypeSourceViewer-ChbPy-OM.js";import"./react-is-Clcustum.js";import"./styled-components-B3ojS1U6.js";import"./@tanstack/react-virtual-B10yMsUP.js";import"./tslib-DoU9Jm1N.js";function a(i){const t={h1:"h1",h2:"h2",...n(),...i.components};return e.jsxs(e.Fragment,{children:[e.jsx(o,{title:"Композиции/PageTitle/API"}),`
-`,e.jsx(t.h1,{id:"pagetitle-api",children:"PageTitle API"}),`
-`,e.jsx(t.h2,{id:"pagetitleprops",children:"PageTitleProps"}),`
-`,e.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/components/PageTitle/PageTitle.types.ts",typeName:"PageTitleProps"}),`
-`,e.jsx(t.h2,{id:"pagetitleslotsizesprops",children:"PageTitleSlotSizesProps"}),`
-`,e.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/components/PageTitle/PageTitle.types.ts",typeName:"PageTitleSlotSizesProps"}),`
-`,e.jsx(t.h2,{id:"pagetitleadaptiveslot",children:"PageTitleAdaptiveSlot"}),`
-`,e.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/components/PageTitle/PageTitle.types.ts",typeName:"PageTitleAdaptiveSlot"})]})}function h(i={}){const{wrapper:t}={...n(),...i.components};return t?e.jsx(t,{...i,children:e.jsx(a,{...i})}):a(i)}export{h as default};
--- pr-preview/pr-74/assets/rehype-sanitize-CmElev6w.js
+++
@@ -1,1 +0,0 @@
-import{bz as e}from"./vendor-BpZdl-X2.js";function o(r){return function(t){return e(t,r)}}export{o as r};
