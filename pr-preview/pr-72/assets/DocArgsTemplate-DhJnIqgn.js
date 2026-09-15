8f6c6b1dda55c28e4576a2c75eccb1a7f93ff27a
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-15 06:10:09 +0000

    Remove preview for PR 72 🛬

--- pr-preview/pr-72/assets/DocArgsTemplate-DhJnIqgn.js
+++
@@ -1,6 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{cg as o,ch as s,c9 as i,cb as a}from"./vendor-D8axKApy.js";import"./react-is-Clcustum.js";import"./styled-components-CDkh0cjP.js";import"./@tanstack/react-virtual-BDT2t9w4.js";import"./tslib-DoU9Jm1N.js";function r(n){return t.jsxs(t.Fragment,{children:[`
-`,`
-`,`
-`,t.jsx(s,{isTemplate:!0}),`
-`,t.jsx(i,{}),`
-`,t.jsx(a,{})]})}function f(n={}){const{wrapper:e}={...o(),...n.components};return e?t.jsx(e,{...n,children:t.jsx(r,{...n})}):r()}export{f as default};
--- pr-preview/pr-72/assets/SwitchAPI-CTVamymF.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{cg as o,ch as c}from"./vendor-D8axKApy.js";import{T as i}from"./TypeSourceViewer-DoJu92sb.js";import"./react-is-Clcustum.js";import"./styled-components-CDkh0cjP.js";import"./@tanstack/react-virtual-BDT2t9w4.js";import"./tslib-DoU9Jm1N.js";function n(s){const e={blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",p:"p",...o(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(c,{title:"Локальные компоненты/Switch/API"}),`
-`,t.jsx(e.h1,{id:"switch-api",children:"Switch API"}),`
-`,t.jsx(e.h2,{id:"baseswitchprops",children:"BaseSwitchProps"}),`
-`,t.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/components/Switch/types.ts",typeName:"BaseSwitchProps"}),`
-`,t.jsxs(e.blockquote,{children:[`
-`,t.jsxs(e.p,{children:["Остальные пропсы наследуются от ",t.jsx(e.code,{children:"Switch"})," из ",t.jsx(e.code,{children:"@salutejs/sdds-finai"})]}),`
-`]})]})}function u(s={}){const{wrapper:e}={...o(),...s.components};return e?t.jsx(e,{...s,children:t.jsx(n,{...s})}):n(s)}export{u as default};
