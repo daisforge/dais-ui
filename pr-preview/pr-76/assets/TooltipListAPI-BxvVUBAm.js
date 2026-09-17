72b319e926c7e9f12f3643735a749a780a778e0e
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-17 09:03:24 +0000

    Remove preview for PR 76 🛬

--- pr-preview/pr-76/assets/TooltipListAPI-BxvVUBAm.js
+++
@@ -1,7 +0,0 @@
-import{j as o}from"./react-D2T61mpp.js";import{cg as s,ch as n}from"./vendor-B11kNQfP.js";import{T as r}from"./TypeSourceViewer-DaZe4noS.js";import"./react-is-Clcustum.js";import"./styled-components-C0zA7hwz.js";import"./@tanstack/react-virtual-DXBBqCVI.js";import"./tslib-DoU9Jm1N.js";function i(e){const t={blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",p:"p",...s(),...e.components};return o.jsxs(o.Fragment,{children:[o.jsx(n,{title:"Локальные компоненты/TooltipList/API"}),`
-`,o.jsx(t.h1,{id:"tooltiplist-api",children:"TooltipList API"}),`
-`,o.jsx(t.h2,{id:"tooltiplistprops",children:"TooltipListProps"}),`
-`,o.jsx(r,{language:"ts",filePath:"packages/ui-kit/src/components/Tooltip/TooltipList.tsx",typeName:"TooltipListProps"}),`
-`,o.jsxs(t.blockquote,{children:[`
-`,o.jsxs(t.p,{children:["Остальные пропсы наследуются от ",o.jsx(t.code,{children:"TooltipProps"})," (кроме ",o.jsx(t.code,{children:"text"}),", ",o.jsx(t.code,{children:"children"}),")"]}),`
-`]})]})}function x(e={}){const{wrapper:t}={...s(),...e.components};return t?o.jsx(t,{...e,children:o.jsx(i,{...e})}):i(e)}export{x as default};
