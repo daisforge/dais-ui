e41fdd74afc0b62e9ee562332b11adad0a602ed1
RamK-16 <RamK-16@users.noreply.github.com>
2026-09-18 13:24:07 +0000

    Remove preview for PR 83 🛬

--- pr-preview/pr-83/assets/LayoutAPI-CdzwPG48.js
+++
@@ -1,8 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{cg as n,ch as u}from"./vendor-BRS_RCGD.js";import{T as s}from"./TypeSourceViewer-f3cT8fAM.js";import"./react-is-Clcustum.js";import"./styled-components-BDsUHzM4.js";import"./@tanstack/react-virtual-BOKlFDDB.js";import"./tslib-DoU9Jm1N.js";function o(e){const a={h1:"h1",h2:"h2",...n(),...e.components};return t.jsxs(t.Fragment,{children:[t.jsx(u,{title:"Композиции/Layout/API"}),`
-`,t.jsx(a.h1,{id:"layout-api",children:"Layout API"}),`
-`,t.jsx(a.h2,{id:"layoutprops",children:"LayoutProps"}),`
-`,t.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/layouts/Layout/Layout.types.ts",typeName:"LayoutProps"}),`
-`,t.jsx(a.h2,{id:"layoutclasses",children:"LayoutClasses"}),`
-`,t.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/layouts/Layout/Layout.types.ts",typeName:"LayoutClasses"}),`
-`,t.jsx(a.h2,{id:"layoutspacing-deprecated",children:"LayoutSpacing (deprecated)"}),`
-`,t.jsx(s,{language:"ts",filePath:"packages/ui-kit/src/layouts/Layout/Layout.types.ts",typeName:"LayoutSpacing"})]})}function h(e={}){const{wrapper:a}={...n(),...e.components};return a?t.jsx(a,{...e,children:t.jsx(o,{...e})}):o(e)}export{h as default};
