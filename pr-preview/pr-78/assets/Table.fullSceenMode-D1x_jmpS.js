f79edb2d7f0d65091608993712637911afcbad17
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-18 09:03:40 +0000

    Deploy preview for PR 78 🛫

---
+++ pr-preview/pr-78/assets/Table.fullSceenMode-D1x_jmpS.js
@@ -0,0 +1,11 @@
+import{j as r}from"./react-D2T61mpp.js";import{cg as n,ch as e,ca as m}from"./vendor-DwzXrIa_.js";import{T as p}from"./Table.fullScreenMode.stories-BjD-i3A7.js";import"./react-is-Clcustum.js";import"./styled-components-C4HVP9Bu.js";import"./@tanstack/react-virtual-B5rjj9YJ.js";import"./tslib-DoU9Jm1N.js";import"./tableData-DVJFoYoT.js";import"./DocStoryTemplate-D1KCoccP.js";import"./storySourceDoc-tVKyHcEN.js";import"./Table-CIOD-j4A.js";import"./FiltersActions-8i4OR0vz.js";import"./IconButton-BdMspFUd.js";import"./@salutejs/plasma-icons-ByXz74TC.js";import"./@salutejs/sdds-finai-CLAU31SQ.js";import"./@salutejs/sdds-themes-p9DCXULv.js";import"./utils-Cam_MtZy.js";import"./constants-Ci5uyz-N.js";import"./sharedUtilsDebug-BX_KjCjW.js";import"./Box-C2zw6gNX.js";import"./TextField-BKiYQwUB.js";import"./sharedUtilsInputs-Bi3qGV92.js";import"./AnalyticalWidget-pSEUAb11.js";import"./Collapse-DWYGfMp3.js";import"./react-data-grid-BhThHQcM.js";import"./TableTabs-_rcOlpWb.js";import"./TableCanvasSharedConstants-B2qJZwC8.js";import"./sharedUiSearch-DQvbKNpP.js";import"./ListOfFilters-BYLKJCes.js";import"./lodash.isequal-DD0Lfcik.js";import"./NumberFormat-Cqbr1Qf0.js";import"./EmptyState-xzg7F41C.js";import"./MassActions-C-NecXAT.js";import"./Autocomplete-kr3RqShP.js";function i(t){const o={a:"a",h1:"h1",h2:"h2",li:"li",p:"p",strong:"strong",ul:"ul",...n(),...t.components};return r.jsxs(r.Fragment,{children:[r.jsx(e,{of:p,name:"Docs"}),`
+`,r.jsx(o.h1,{id:"tablefullscreenmode",children:"TableFullScreenMode"}),`
+`,r.jsx(o.h2,{id:"ключевые-особенности",children:"Ключевые особенности"}),`
+`,r.jsxs(o.ul,{children:[`
+`,r.jsx(o.li,{children:"Включение полноэкранного режима"}),`
+`,r.jsx(o.li,{children:"Поддержка дефолтно открытого состояния"}),`
+`,r.jsx(o.li,{children:"Возможность внешнего управления состоянием"}),`
+`]}),`
+`,r.jsx(o.p,{children:r.jsx(o.strong,{children:"tableConfig.fullScreenEnabled"})}),`
+`,r.jsxs(o.p,{children:["Описание типов - в разделе ",r.jsx(o.a,{href:"?path=/docs/%D0%BB%D0%BE%D0%BA%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D1%8B-table-fullscreenmode-api--docs",children:"API"}),"."]}),`
+`,r.jsx(m,{})]})}function H(t={}){const{wrapper:o}={...n(),...t.components};return o?r.jsx(o,{...t,children:r.jsx(i,{...t})}):i(t)}export{H as default};
---
+++ pr-preview/pr-78/assets/Table.infinityScroll-C58lKcYz.js
@@ -0,0 +1,15 @@
+import{j as i}from"./react-D2T61mpp.js";import{cg as t,ch as e,ca as s}from"./vendor-DwzXrIa_.js";import{T as l}from"./Table.infinityScroll.stories-CGkMEFsP.js";import"./react-is-Clcustum.js";import"./styled-components-C4HVP9Bu.js";import"./@tanstack/react-virtual-B5rjj9YJ.js";import"./tslib-DoU9Jm1N.js";import"./tableData-DVJFoYoT.js";import"./DocStoryTemplate-D1KCoccP.js";import"./storySourceDoc-tVKyHcEN.js";import"./Table-CIOD-j4A.js";import"./FiltersActions-8i4OR0vz.js";import"./IconButton-BdMspFUd.js";import"./@salutejs/plasma-icons-ByXz74TC.js";import"./@salutejs/sdds-finai-CLAU31SQ.js";import"./@salutejs/sdds-themes-p9DCXULv.js";import"./utils-Cam_MtZy.js";import"./constants-Ci5uyz-N.js";import"./sharedUtilsDebug-BX_KjCjW.js";import"./Box-C2zw6gNX.js";import"./TextField-BKiYQwUB.js";import"./sharedUtilsInputs-Bi3qGV92.js";import"./AnalyticalWidget-pSEUAb11.js";import"./Collapse-DWYGfMp3.js";import"./react-data-grid-BhThHQcM.js";import"./TableTabs-_rcOlpWb.js";import"./TableCanvasSharedConstants-B2qJZwC8.js";import"./sharedUiSearch-DQvbKNpP.js";import"./ListOfFilters-BYLKJCes.js";import"./lodash.isequal-DD0Lfcik.js";import"./NumberFormat-Cqbr1Qf0.js";import"./EmptyState-xzg7F41C.js";import"./MassActions-C-NecXAT.js";import"./Autocomplete-kr3RqShP.js";function n(o){const r={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...t(),...o.components};return i.jsxs(i.Fragment,{children:[i.jsx(e,{of:l,name:"Docs"}),`
+`,i.jsx(r.h1,{id:"infinityscroll",children:"InfinityScroll"}),`
+`,i.jsxs(r.p,{children:["Бесконечная подгрузка данных при прокрутке legacy ",i.jsx(r.code,{children:"Table"}),"."]}),`
+`,i.jsx(r.h2,{id:"ключевые-особенности",children:"Ключевые особенности"}),`
+`,i.jsxs(r.ul,{children:[`
+`,i.jsxs(r.li,{children:["Включается наличием ",i.jsx(r.code,{children:"tableConfig.infinityScroll"})]}),`
+`,i.jsxs(r.li,{children:[i.jsx(r.code,{children:"onTrigger"})," вызывается при достижении нижней границы"]}),`
+`,i.jsxs(r.li,{children:[i.jsx(r.code,{children:"isLoading"})," блокирует повторный вызов во время загрузки"]}),`
+`,i.jsxs(r.li,{children:[i.jsx(r.code,{children:"hasMore"})," управляет наличием следующей страницы"]}),`
+`,i.jsxs(r.li,{children:[i.jsx(r.code,{children:"scrollThreshold"})," задает расстояние до низа для раннего запуска"]}),`
+`]}),`
+`,i.jsx(r.h2,{id:"особенности",children:"Особенности"}),`
+`,i.jsx(r.p,{children:"При активном infinity scroll таблица не применяет дефолтные сортировку и фильтрацию, потому что на клиенте обычно находится не весь набор данных."}),`
+`,i.jsxs(r.p,{children:["Описание типов - в разделе ",i.jsx(r.a,{href:"?path=/docs/%D0%BB%D0%BE%D0%BA%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D1%8B-table-infinityscroll-api--docs",children:"API"}),"."]}),`
+`,i.jsx(s,{})]})}function H(o={}){const{wrapper:r}={...t(),...o.components};return r?i.jsx(r,{...o,children:i.jsx(n,{...o})}):n(o)}export{H as default};
