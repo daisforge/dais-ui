eecf9d67ef2d13088725bde92bf728cb64361068
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-24 14:33:58 +0000

    deploy: fb137d77017a5e3885a9232f24337a53f052b425

---
+++ assets/Table.columnsControl-Dww6VVrm.js
@@ -0,0 +1,15 @@
+import{j as o}from"./react-D2T61mpp.js";import{cg as t,ch as e,ca as s}from"./vendor-nNOZyNLP.js";import{T as l}from"./Table.columnsControl.stories-CToRC0KX.js";import"./react-is-Clcustum.js";import"./styled-components-Bm5ATt6S.js";import"./@tanstack/react-virtual-CKihwRsW.js";import"./tslib-DoU9Jm1N.js";import"./tableData-DVJFoYoT.js";import"./DocStoryTemplate-tvb6peuH.js";import"./storySourceDoc-tVKyHcEN.js";import"./Box-WHWAT9BU.js";import"./Table-DRgKo2gz.js";import"./FiltersActions-DHxY0g1R.js";import"./IconButton-BvgWrmFd.js";import"./@salutejs/plasma-icons-C9x_65-Y.js";import"./@salutejs/sdds-finai-D4KNbPPv.js";import"./@salutejs/sdds-themes-p9DCXULv.js";import"./utils-CLSMcwW8.js";import"./constants-Ci5uyz-N.js";import"./sharedUtilsDebug-BX_KjCjW.js";import"./TextField-hqaOplq4.js";import"./sharedUtilsInputs-DnpxamI4.js";import"./AnalyticalWidget-CrgSXtaY.js";import"./Collapse-C4JYT3GB.js";import"./react-data-grid-BS7__oFy.js";import"./TableTabs-BJsmoly0.js";import"./TableCanvasSharedConstants-B2qJZwC8.js";import"./sharedUiSearch-DQxlj_DG.js";import"./ListOfFilters-CMswyx6_.js";import"./lodash.isequal-DD0Lfcik.js";import"./NumberFormat-Cz5fAZak.js";import"./EmptyState-D_jmVDrT.js";import"./MassActions-B9xZCgbn.js";import"./Autocomplete-Chs_PDqY.js";function i(r){const n={a:"a",code:"code",h1:"h1",h2:"h2",li:"li",p:"p",ul:"ul",...t(),...r.components};return o.jsxs(o.Fragment,{children:[o.jsx(e,{of:l,name:"Docs"}),`
+`,o.jsx(n.h1,{id:"columnscontrol",children:"ColumnsControl"}),`
+`,o.jsxs(n.p,{children:["Пользовательская настройка колонок legacy ",o.jsx(n.code,{children:"Table"}),"."]}),`
+`,o.jsx(n.h2,{id:"ключевые-особенности",children:"Ключевые особенности"}),`
+`,o.jsxs(n.ul,{children:[`
+`,o.jsxs(n.li,{children:["Включение через ",o.jsx(n.code,{children:"tableConfig.columnsControl.enable"})]}),`
+`,o.jsx(n.li,{children:"Скрытие, закрепление и переупорядочивание колонок"}),`
+`,o.jsx(n.li,{children:"Запрет скрытия или закрепления отдельных колонок"}),`
+`,o.jsxs(n.li,{children:["Дефолтные состояния ",o.jsx(n.code,{children:"orderDefault"}),", ",o.jsx(n.code,{children:"hiddenDefault"}),", ",o.jsx(n.code,{children:"pinnedDefault"})]}),`
+`,o.jsxs(n.li,{children:["Колбэк ",o.jsx(n.code,{children:"onConfirm"})," для сохранения настроек вне таблицы"]}),`
+`]}),`
+`,o.jsx(n.h2,{id:"особенности",children:"Особенности"}),`
+`,o.jsxs(n.p,{children:[o.jsx(n.code,{children:"columnsControl"})," может работать вместе с изменением ширины колонок и с ",o.jsx(n.code,{children:"keyText"}),". Для группированных колонок проверяйте итоговый порядок leaf-колонок."]}),`
+`,o.jsxs(n.p,{children:["Описание типов - в разделе ",o.jsx(n.a,{href:"?path=/docs/%D0%BB%D0%BE%D0%BA%D0%B0%D0%BB%D1%8C%D0%BD%D1%8B%D0%B5-%D0%BA%D0%BE%D0%BC%D0%BF%D0%BE%D0%BD%D0%B5%D0%BD%D1%82%D1%8B-table-columnscontrol-api--docs",children:"API"}),"."]}),`
+`,o.jsx(s,{})]})}function H(r={}){const{wrapper:n}={...t(),...r.components};return n?o.jsx(n,{...r,children:o.jsx(i,{...r})}):i(r)}export{H as default};
