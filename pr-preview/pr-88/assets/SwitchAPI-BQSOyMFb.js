2625b3e6f590ba18f7ad2a6f57e756726f203712
RamK-16 <RamK-16@users.noreply.github.com>
2026-09-24 10:35:52 +0000

    Remove preview for PR 88 🛬

--- pr-preview/pr-88/assets/SwitchAPI-BQSOyMFb.js
+++
@@ -1,7 +0,0 @@
-import{j as t}from"./react-D2T61mpp.js";import{cg as o,ch as c}from"./vendor-CKeqW2ds.js";import{T as i}from"./TypeSourceViewer-LTWBftvA.js";import"./react-is-Clcustum.js";import"./styled-components-BxBKAjOg.js";import"./@tanstack/react-virtual-B43JA17u.js";import"./tslib-DoU9Jm1N.js";function n(s){const e={blockquote:"blockquote",code:"code",h1:"h1",h2:"h2",p:"p",...o(),...s.components};return t.jsxs(t.Fragment,{children:[t.jsx(c,{title:"Локальные компоненты/Switch/API"}),`
-`,t.jsx(e.h1,{id:"switch-api",children:"Switch API"}),`
-`,t.jsx(e.h2,{id:"baseswitchprops",children:"BaseSwitchProps"}),`
-`,t.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/components/Switch/types.ts",typeName:"BaseSwitchProps"}),`
-`,t.jsxs(e.blockquote,{children:[`
-`,t.jsxs(e.p,{children:["Остальные пропсы наследуются от ",t.jsx(e.code,{children:"Switch"})," из ",t.jsx(e.code,{children:"@salutejs/sdds-finai"})]}),`
-`]})]})}function u(s={}){const{wrapper:e}={...o(),...s.components};return e?t.jsx(e,{...s,children:t.jsx(n,{...s})}):n(s)}export{u as default};
