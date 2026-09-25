e143cd859e998889af0a901ec7f6686b7bde5318
PavelPotapov <PavelPotapov@users.noreply.github.com>
2026-09-25 12:59:16 +0000

    Remove preview for PR 91 🛬

--- pr-preview/pr-91/assets/Table.collapsingAPI-BVA7wLvb.js
+++
@@ -1,4 +0,0 @@
-import{j as e}from"./react-D2T61mpp.js";import{cg as s,ch as a}from"./vendor-C4kmGUJR.js";import{T as i}from"./TypeSourceViewer-DtYWZlDQ.js";import"./react-is-Clcustum.js";import"./styled-components-igVELayG.js";import"./@tanstack/react-virtual-tAdAmKr0.js";import"./tslib-DoU9Jm1N.js";function o(t){const n={h1:"h1",h2:"h2",...s(),...t.components};return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Локальные компоненты/Table/Collapsing/API"}),`
-`,e.jsx(n.h1,{id:"collapsing-api",children:"Collapsing API"}),`
-`,e.jsx(n.h2,{id:"tablecollapseconfig",children:"TableCollapseConfig"}),`
-`,e.jsx(i,{language:"ts",filePath:"packages/ui-kit/src/components/Table/types/table-config.type.ts",typeName:"TableCollapseConfig"})]})}function f(t={}){const{wrapper:n}={...s(),...t.components};return n?e.jsx(n,{...t,children:e.jsx(o,{...t})}):o(t)}export{f as default};
