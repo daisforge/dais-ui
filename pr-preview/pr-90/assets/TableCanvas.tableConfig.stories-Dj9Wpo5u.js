7a3c36c556ac3edaea1850f88c20bdc10e5758fa
RamK-16 <RamK-16@users.noreply.github.com>
2026-09-25 07:33:09 +0000

    Remove preview for PR 90 🛬

--- pr-preview/pr-90/assets/TableCanvas.tableConfig.stories-Dj9Wpo5u.js
+++
@@ -1,5 +0,0 @@
-import{d as o}from"./react-D2T61mpp.js";import t from"./DocArgsTemplate-B_jElDeD.js";import{c as n}from"./StoriesUtils-dbm_M7P3.js";import"./vendor-DnKKx4bH.js";import"./react-is-Clcustum.js";import"./styled-components-C073gJi0.js";import"./@tanstack/react-virtual-B5-u2qiU.js";import"./tslib-DoU9Jm1N.js";const b={title:"Локальные компоненты/TableCanvas/API/tableConfig",parameters:{docs:{page:t,description:{component:"### API tableConfig:"}}},component:n},e={name:"API",render:()=>o.jsxDEV(o.Fragment,{},void 0,!1,{fileName:"/home/runner/work/dais-ui/dais-ui/packages/storybook/src/stories/TableCanvas/API/TableCanvas.tableConfig.stories.tsx",lineNumber:25,columnNumber:17},void 0)};var r,a,s;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`{
-  name: 'API',
-  // eslint-disable-next-line react/jsx-no-useless-fragment
-  render: () => <></>
-}`,...(s=(a=e.parameters)==null?void 0:a.docs)==null?void 0:s.source}}};const C=["Docs"];export{e as Docs,C as __namedExportsOrder,b as default};
